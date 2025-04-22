339329cb56751a0a75a76e3044949b9dafbab1a0
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2025-04-22 19:44:21 -0300

    chore: update package configuration and add visualizer plugin
    - Updated package.json to include new types and entry points for hooks and components
    - Added rollup-plugin-visualizer to Vite configuration for better build insights
    - Enhanced Vite build configuration to support multiple entry points and improved output structure

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,46 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import path from 'node:path'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    visualizer({
+      filename: 'dist/stats.html',
+      gzipSize: true,
+      brotliSize: true,
+      template: 'flamegraph'
+    })
+  ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
-      name: 'SecretLib',
+      name: 'secret-lib',
+      entry: {
+        index: path.resolve(__dirname, 'lib/index.ts'),
+        hooks: path.resolve(__dirname, 'lib/hooks/index.ts'),
+        tooltip: path.resolve(__dirname, 'lib/components/tooltip/index.ts'),
+        tags: path.resolve(__dirname, 'lib/components/tags/index.ts')
+      },
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`
+      fileName: (format, entryName) =>
+        entryName === 'index'
+          ? `index.${format === 'es' ? 'mjs' : 'cjs'}`
+          : `lib/components/${entryName}/index.${format === 'es' ? 'mjs' : 'cjs'}`
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'tailwindcss', 'clsx', 'tailwind-merge']
+      external: [
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+        'tailwindcss',
+        'clsx',
+        'tailwind-merge'
+      ],
+      output: {
+        chunkFileNames: 'chunks/[name]-[hash].js'
+      }
     },
     sourcemap: true,
     outDir: 'dist',
