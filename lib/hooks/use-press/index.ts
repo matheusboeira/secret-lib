a2eb2e92f9eeab0c668a0ebaa769bde8a59c0e3c
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2025-04-27 09:51:15 -0300

    feat(usePress): extend functionality with onPressStart and onPressEnd handlers
    - Added support for `onPressStart` and `onPressEnd` event handlers to the `usePress` hook for enhanced press event management.
    - Updated type definitions to include new handlers, improving flexibility for users of the hook.
    - Refactored event handling logic to incorporate the new handlers for click, touch, and keyboard events.

--- lib/hooks/use-press/index.ts
+++ lib/hooks/use-press/index.ts
@@ -2,16 +2,32 @@ import { useCallback } from 'react'
 import { useCallbackRefs } from '../use-callback-refs'
 
 export type PressEvent = React.SyntheticEvent
+export type PressEventHandler = (event?: PressEvent) => void
 
 type UsePressProps = {
-  onPress?: (event?: PressEvent) => void
+  onPress?: PressEventHandler
+  onPressStart?: PressEventHandler
+  onPressEnd?: PressEventHandler
   onKeyDown?: (event: React.KeyboardEvent) => void
   tabIndex?: number
 }
 
-export const usePress = ({ onPress, onKeyDown, tabIndex }: UsePressProps) => {
-  const { onPress: onPressRef, onKeyDown: onKeyDownRef } = useCallbackRefs({
+export const usePress = ({
+  onPress,
+  onPressStart,
+  onPressEnd,
+  onKeyDown,
+  tabIndex
+}: UsePressProps) => {
+  const {
+    onPress: onPressRef,
+    onPressStart: onPressStartRef,
+    onPressEnd: onPressEndRef,
+    onKeyDown: onKeyDownRef
+  } = useCallbackRefs({
     onPress,
+    onPressStart,
+    onPressEnd,
     onKeyDown
   })
 
@@ -19,31 +35,44 @@ export const usePress = ({ onPress, onKeyDown, tabIndex }: UsePressProps) => {
     (event: React.KeyboardEvent) => {
       if (event.key === 'Enter' || event.key === ' ') {
         event.preventDefault()
+        onPressStartRef?.(event)
         onPressRef?.(event)
+        onPressEndRef?.(event)
       }
       onKeyDownRef?.(event)
     },
-    [onPressRef, onKeyDownRef]
+    [onPressRef, onPressStartRef, onPressEndRef, onKeyDownRef]
   )
 
   const handleClick = useCallback(
     (event: React.MouseEvent) => {
       event.preventDefault()
+      onPressStartRef?.(event)
       onPressRef?.(event)
+      onPressEndRef?.(event)
     },
-    [onPressRef]
+    [onPressRef, onPressStartRef, onPressEndRef]
+  )
+
+  const handleTouchStart = useCallback(
+    (event: React.TouchEvent) => {
+      onPressStartRef?.(event)
+    },
+    [onPressStartRef]
   )
 
   const handleTouchEnd = useCallback(
     (event: React.TouchEvent) => {
       event.preventDefault()
       onPressRef?.(event)
+      onPressEndRef?.(event)
     },
-    [onPressRef]
+    [onPressRef, onPressEndRef]
   )
 
   return {
     onClick: handleClick,
+    onTouchStart: handleTouchStart,
     onTouchEnd: handleTouchEnd,
     onKeyDown: handleKeyDown,
     tabIndex: tabIndex ?? 0,
